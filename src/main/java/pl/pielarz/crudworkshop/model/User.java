39ea5b0c8f49418f0d942ccadc6c379f4b76199a
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-19 18:33:55 +0200

    User entity

---
+++ src/main/java/pl/pielarz/crudworkshop/model/User.java
@@ -0,0 +1,109 @@
+package pl.pielarz.crudworkshop.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="pesel")
+    private String pesel;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @Transient
+    private Integer daysOfHoliday;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getDaysOfHoliday() {
+        return daysOfHoliday;
+    }
+
+    public void setDaysOfHoliday(Integer daysOfHoliday) {
+        this.daysOfHoliday = daysOfHoliday;
+    }
+}
