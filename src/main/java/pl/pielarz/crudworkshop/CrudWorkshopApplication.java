01eee222d8b6a26294b523cfe6f09530f59c1fa4
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-19 17:52:52 +0200

    first commit

---
+++ src/main/java/pl/pielarz/crudworkshop/CrudWorkshopApplication.java
@@ -0,0 +1,13 @@
+package pl.pielarz.crudworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pielarz/crudworkshop/CrudWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pielarz.crudworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
